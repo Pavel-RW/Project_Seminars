5642ad442f1c3d4848542cad28d9b79802de900d
Skipper <ALPskipper@gmail.com>
2023-05-26 02:44:37 +0300

    Task03 updated

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -4,37 +4,70 @@
 // 5 -> Пятница
 
 Console.WriteLine("Enter the day of the week");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number >= 1 && number <= 7)
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number >= 1 && number <= 7)
+// {
+//     if (number == 1)
+//     {
+//         Console.WriteLine("Monday");
+//     }
+//     else if (number == 2)
+//     {
+//         Console.WriteLine("Tuesday");
+//     }
+//     else if (number == 3)
+//     {
+//         Console.WriteLine("Wednesday");
+//     }
+//     else if (number == 4)
+//     {
+//         Console.WriteLine("Thursday");
+//     }
+//     else if (number == 5)
+//     {
+//         Console.WriteLine("Friday");
+//     }
+//     else if (number == 6)
+//     {
+//         Console.WriteLine("Saturday");
+//     }
+//     else if (number == 7)
+//     {
+//         Console.WriteLine("Sunday");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Wrong day of the week!!!");
+// }
+string number = Console.ReadLine();
+if (number == "1")
 {
-    if (number == 1)
-    {
-        Console.WriteLine("Monday");
-    }
-    else if (number == 2)
-    {
-        Console.WriteLine("Tuesday");
-    }
-    else if (number == 3)
-    {
-        Console.WriteLine("Wednesday");
-    }
-    else if (number == 4)
-    {
-        Console.WriteLine("Thursday");
-    }
-    else if (number == 5)
-    {
-        Console.WriteLine("Friday");
-    }
-    else if (number == 6)
-    {
-        Console.WriteLine("Saturday");
-    }
-    else if (number == 7)
-    {
-        Console.WriteLine("Sunday");
-    }
+    Console.WriteLine("Monday");
+}
+else if (number == "2")
+{
+    Console.WriteLine("Tuesday");
+}
+else if (number == "3")
+{
+    Console.WriteLine("Wednesday");
+}
+else if (number == "4")
+{
+    Console.WriteLine("Thursday");
+}
+else if (number == "5")
+{
+    Console.WriteLine("Friday");
+}
+else if (number == "6")
+{
+    Console.WriteLine("Saturday");
+}
+else if (number == "7")
+{
+    Console.WriteLine("Sunday");
 }
 else
 {
