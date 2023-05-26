bcf141fc30b68216c4e6e45cb30ad63ce30817a2
Skipper <ALPskipper@gmail.com>
2023-05-26 03:40:20 +0300

    Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,18 @@
+﻿// 7. Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("Enter three-digit integer");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <= 999)
+{
+int LastDigit = number % 10;
+Console.WriteLine($"{LastDigit} last digit of the number {number}.");
+}
+else
+{
+Console.WriteLine("Invalid number");
+}
