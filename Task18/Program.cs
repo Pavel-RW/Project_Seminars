db5db5df426bb5656085109c7ace14a0dc93daaf
Skipper <ALPskipper@gmail.com>
2023-05-30 14:56:28 +0300

    Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,21 @@
+﻿// адача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Enter quarter number: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+string range = Quarter(quarter);
+string result = range == null
+? "Incorrect quarter number entered"
+: $"Range possible coordinates specified quarter: {range}";
+Console.WriteLine(result);
+
+string Quarter(int q)
+{
+if (q == 1) return "x > 0 ; y > 0";
+if (q == 2) return "x < 0 ; y > 0";
+if (q == 3) return "x < 0 ; y < 0";
+if (q == 4) return "x > 0 ; y < 0";
+return null;
+}
