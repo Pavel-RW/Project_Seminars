cf6f257cd5c375fe0fb65c71df5efea29f16e2b7
Skipper <ALPskipper@gmail.com>
2023-05-30 15:02:45 +0300

    Task18 new add

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -3,19 +3,19 @@
 // возможных координат точек в этой четверти (x и y).
 
 Console.WriteLine("Enter quarter number: ");
-int quarter = Convert.ToInt32(Console.ReadLine());
+string quarter = Console.ReadLine();
 
-string range = Quarter(quarter);
+string range = Range(quarter);
 string result = range == null
 ? "Incorrect quarter number entered"
 : $"Range possible coordinates specified quarter: {range}";
 Console.WriteLine(result);
 
-string Quarter(int q)
+string Range(string q)
 {
-if (q == 1) return "x > 0 ; y > 0";
-if (q == 2) return "x < 0 ; y > 0";
-if (q == 3) return "x < 0 ; y < 0";
-if (q == 4) return "x > 0 ; y < 0";
-return null;
+    if (q == "1") return "x > 0 ; y > 0";
+    if (q == "2") return "x < 0 ; y > 0";
+    if (q == "3") return "x < 0 ; y < 0";
+    if (q == "4") return "x > 0 ; y < 0";
+    return null;
 }
