6ffb2fc849a6d8b2d110124dd02f17f736a7f751
Skipper <ALPskipper@gmail.com>
2023-06-13 18:58:37 +0300

    Task40 and Task42

---
+++ Task40/Program.cs
@@ -0,0 +1,31 @@
+﻿// 40.Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+
+int GetNumber()
+{
+    Console.Write("Enter length side triangle: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0) return number;
+    else return 0;
+}
+
+bool SuchTriangleExists(int a, int b, int c)
+{
+    return (a < b + c) && (b < a + c) && (c < a + b);
+}
+
+int side1 = GetNumber();
+int side2 = GetNumber();
+int side3 = GetNumber();
+
+if (side1 == 0 || side2 == 0 || side3 == 0)
+{
+    Console.WriteLine("Incorrect input");
+    return;
+}
+
+Console.WriteLine(SuchTriangleExists(side1, side2, side3) ? "Triangle exists" : "Triangle not exist");
---
+++ Task42/Program.cs
@@ -0,0 +1,37 @@
+﻿// 42.Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 46 -> 101110
+// 13 -> 1101
+// 2 -> 10
+
+
+int GetNumber()
+{
+    Console.Write("Enter number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number >= 0) return number;
+    else return -1;
+}
+
+int ConvertToBin(int num)
+{
+    int binNum = 0;
+    int mult = 1;
+    while (num > 0)
+    {
+        binNum = binNum + (num % 2 * mult); // 1 + (1 * 10) = 11
+        mult = mult * 10;
+        num = num / 2;
+    }
+    return binNum;
+}
+
+int number = GetNumber();
+
+if (number == -1)
+{
+    Console.WriteLine("Number not negative");
+    return;
+}
+
+Console.WriteLine($"Number {number} binary representation -> {ConvertToBin(number)}");
