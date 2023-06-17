726543f8d412aa03bcd90e33668812ff3cbc3186
Skipper <ALPskipper@gmail.com>
2023-06-17 16:55:00 +0300

    Task41 and Task43

---
+++ Task41/Program.cs
@@ -0,0 +1,53 @@
+﻿// 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+int[] CreateFillArray(int M)
+{
+    int[] arr = new int[M];
+
+    for (int i = 0; i < M; i++)
+    {
+        Console.Write($"Enter number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        arr[i] = number;
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    int length = arr.Length;
+    Console.Write("[");
+    for (int i = 0; i < length; i++)
+    {
+        if (length - i > 1)
+            Console.Write($"{arr[i]}, ");
+        else
+            Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int CountPositiveNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Write("Enter quantity number: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] arr1 = CreateFillArray(M);
+int count = CountPositiveNumbers(arr1);
+PrintArray(arr1);
+Console.Write($"Quantity number more 0: {count}");
---
+++ Task43/Program.cs
@@ -0,0 +1,41 @@
+﻿// 43.Напишите программу, которая найдёт точку пересечения двух прямых,
+//  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//  значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+double b1 = ReadConsole("Enter b1:");
+double k1 = ReadConsole("Enter k1:");
+double b2 = ReadConsole("Enter b2:");
+double k2 = ReadConsole("Enter k2:");
+
+if (k1 == k2 && b1 == b2)
+{
+    Console.WriteLine("Error: lines coincide.");
+    return;
+}
+else if (k1 == k2)
+{
+    Console.WriteLine("Error: lines are parallel.");
+    return;
+}
+
+double[] intersectionPoint = IntersectionPoint(b1, k1, b2, k2);
+double x = Math.Round(intersectionPoint[0], 1);
+double y = Math.Round(intersectionPoint[1], 1);
+Console.WriteLine($"IntersectionPoint: ({x}; {y})");
+
+double ReadConsole(string prompt)
+{
+    Console.Write(prompt + " ");
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double[] IntersectionPoint(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    double[] result = { x, y };
+    return result;
+}
